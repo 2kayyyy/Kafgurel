91e71662905f001184835506766bd3fdaef5468a
2kayyyy <aryankafle004@gmail.com>
2024-05-15 18:30:39 -0400

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def main():
     st.title("Hello Streamlit!")
     name = st.text_input("Enter your name:")
     if st.button("Greet"):
-        st.write(f"Hello, {name}!")
+        st.write(f"Hello mf, {name}!")
 
 if __name__ == "__main__":
     main()
