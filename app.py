21c6e98674a097800ae06271b93fbe43175d653e
Your Name <you@example.com>
2024-05-16 05:21:52 -0400

    ee

--- app.py
+++ app.py
@@ -22,12 +22,12 @@ def git_commit_and_push(file_path, commit_message):
     # Retrieve GitHub credentials from environment variables
     username = os.getenv('GITHUB_USERNAME')
     token = os.getenv('GITHUB_TOKEN')
-    repo_name = "Kafgurel"  # Replace with your repository name
+    repo_name = "Kafgurel"
 
     if username and token:
         repo_url = f"https://{username}:{token}@github.com/{username}/{repo_name}.git"
-        subprocess.run(['git', 'config', '--global', 'user.email', '"you@example.com"'])
-        subprocess.run(['git', 'config', '--global', 'user.name', '"Your Name"'])
+        subprocess.run(['git', 'config', '--global', 'user.email', 'aryankafle004@gmail.com'])
+        subprocess.run(['git', 'config', '--global', 'user.name', '2kayyyy'])
         subprocess.run(['git', 'add', file_path])
         subprocess.run(['git', 'commit', '-m', commit_message])
         subprocess.run(['git', 'push', repo_url])
